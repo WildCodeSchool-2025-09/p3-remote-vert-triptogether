c3595380ccd65ce790990f189b75fdea69d52570
KADRI JUGURTA <jugurtha.kadri@gmail.com>
2026-01-15 19:34:17 +0100

    CreateTrip.css, global.css, reset.css created

--- client/src/pages/CreateTrip.tsx
+++ client/src/pages/CreateTrip.tsx
@@ -1,9 +1,22 @@
+import { useNavigate } from "react-router";
 import CreateTripForm from "../components/CreateTripForm";
 import "../styles/CreateTrip.css";
 
 export default function CreateTrip() {
+  const navigate = useNavigate();
+
   return (
     <div className="create-trip-page">
+      <button
+        type="button"
+        className="button-back-arrow"
+        onClick={() => navigate(-1)}
+      >
+        <img
+          src="../src/assets/images/back-arrow-logo.png"
+          alt="flèche de retour arrière"
+        />
+      </button>
       <img src="/logos/logo-airplane.png" alt="logo-avion" />
       <h1>Créer un nouveau voyage</h1>
       <p>Commencez par définir les bases de votre aventure</p>

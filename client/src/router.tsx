1b76fa303d698c035d426b61069e3db3dfa6ee54
Kevin Ressegaire <176156975+earzalien@users.noreply.github.com>
2026-01-26 14:50:34 +0100

    useToast créé

--- client/src/router.tsx
+++ client/src/router.tsx
@@ -2,34 +2,41 @@ import { createBrowserRouter } from "react-router";
 import App from "./App";
 import Invitation from "./pages/Invitation";
 import Membres from "./pages/Membres";
+import { Trip } from "./pages/Trip";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
-  },
-  {
-    path: "/invitation/:id",
-    element: <Invitation />,
-  },
-  {
-    path: "/trip/:id/explorer",
-    element: <Membres />, //TODO CREER COMPOSANT EXPLORER (anciennemnt destination)
-  },
-  {
-    path: "/trip/:id/membres",
-    element: <Membres />,
-  },
-  {
-    path: "/trip/:id/map",
-    element: <Membres />, //TODO CREER COMPOSANT MAP
-  },
-  {
-    path: "/trip/:id/budget",
-    element: <Membres />, //TODO CREER COMPOSANT BUDGET
-  },
-  {
-    path: "/trip/:id/chat",
-    element: <Membres />, //TODO CREER COMPOSANT TCHAT
+    children: [
+      {
+        path: "/invitation/:id",
+        element: <Invitation />,
+      },
+      {
+        path: "/trip/:id",
+        element: <Trip />,
+      },
+      {
+        path: "/trip/:id/explorer",
+        element: <Membres />, //TODO CREER COMPOSANT EXPLORER (anciennemnt destination)
+      },
+      {
+        path: "/trip/:id/membres",
+        element: <Membres />,
+      },
+      {
+        path: "/trip/:id/map",
+        element: <Membres />, //TODO CREER COMPOSANT MAP
+      },
+      {
+        path: "/trip/:id/budget",
+        element: <Membres />, //TODO CREER COMPOSANT BUDGET
+      },
+      {
+        path: "/trip/:id/chat",
+        element: <Membres />, //TODO CREER COMPOSANT TCHAT
+      },
+    ],
   },
 ] as const);

9730fc2370d817144efc2147323daea1fc035549
Uldyssian <mickael.brouttier@gmail.com>
2026-02-12 11:16:04 +0100

    Fix try

--- client/src/components/TripInfos.tsx
+++ client/src/components/TripInfos.tsx
@@ -1,5 +1,5 @@
 import TripCard from "../pages/TripCard";
-import TripInvitation from "../pages/tripInvitations";
+import TripInvitation from "../pages/TripInvitations";
 import type { Trip } from "../types/tripType";
 import Modal from "./Modal";
 import "../pages/styles/TripInfos.css";
